8a561863cfbe486f0407d3f1e8538c964a19b3a7
wwagner4 <wwagner4@gmail.com>
2013-06-01 12:07:18 +0200

    added exaption handling to avoid stuck applications

--- akka-tryout/akka-client-tryout/src/main/scala/net/entelijan/akka/Client.scala
+++ akka-tryout/akka-client-tryout/src/main/scala/net/entelijan/akka/Client.scala
@@ -10,33 +10,40 @@ import ExecutionContext.Implicits.global
 import akka.pattern.AskTimeoutException
 
 object Client extends App {
-  import scala.language.postfixOps
+  try {
+    import scala.language.postfixOps
 
-  val system = ActorSystem("sys")
-  println("--- A")
-  
-  val url = "akka://sys@127.0.0.1:8988/user/server"
-  
-  val a = system.actorFor(url)
-  
-  val f  = a.ask(Status)(2 second) 
-  println(s"--- C1 ${f}")
+    val system = ActorSystem("sys")
+    println("--- A")
+    val url = "akka://sys@127.0.0.1:8988/user/server"
 
-  f onComplete {
-    case Success(x) => {
-      println(s"Completed with Success received '${x}'")
-      val msg = Info("Hello from client")
-      a ! msg
-      println(s"--- C2 ${msg}")
-     }
-     case Failure(x) => println(s"--- C3 Completed with failure ${x}")
+    val a = system.actorFor(url)
+
+    val f = a.ask(Status)(5 second)
+    println(s"--- C1 ${f}")
+
+    f onComplete {
+      case Success(x) => {
+        println(s"Completed with Success received '${x}'")
+        val msg = Info("Hello from client")
+        a ! msg
+        println(s"--- C2 sent message to server: '${msg}'")
+      }
+      case Failure(x) => println(s"--- C3 Completed with failure ${x}")
+    }
+    f onFailure {
+      case at: AskTimeoutException => println(s"--- C4a Received AkkaTimeoutException '${at.getMessage()}' onFailure")
+      case any => println(s"--- C4b Received Any '$any' onFailure")
+    }
+
+    system.shutdown
+    println("--- D shutdown")
+  } catch {
+    case e: Exception => {
+      println(s"--- E Failure in initializing the client $e")
+      println("--- F exit -1")
+      System.exit(-1)
+    }
   }
-  f onFailure {
-    case at: AskTimeoutException => println(s"--- C4a Received AkkaTimeoutException '${at.getMessage()}' onFailure")
-    case any => println(s"--- C4b Received Any '$any' onFailure")
-  }
-    
-  system.shutdown
-  println("--- D shutdown")
 
 }
