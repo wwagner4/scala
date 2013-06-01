6563ec158b9c493cc4a54c094c1597459008c8c6
wwagner4 <wwagner4@gmail.com>
2013-06-01 12:42:28 +0200

    improved timeout handling in client

--- akka-tryout/akka-client-tryout/src/main/scala/net/entelijan/akka/Client.scala
+++ akka-tryout/akka-client-tryout/src/main/scala/net/entelijan/akka/Client.scala
@@ -6,20 +6,22 @@ import scala.concurrent.duration._
 import akka.pattern.ask
 import scala.concurrent._
 import scala.util._
+import akka.util._
 import ExecutionContext.Implicits.global
 import akka.pattern.AskTimeoutException
 
 object Client extends App {
   try {
     import scala.language.postfixOps
-
+    implicit val timeout = Timeout(3 seconds)
+    
     val system = ActorSystem("sys")
     println("--- A")
     val url = "akka://sys@127.0.0.1:8988/user/server"
 
     val a = system.actorFor(url)
 
-    val f = a.ask(Status)(5 second)
+    val f = a ? Status
     println(s"--- C1 ${f}")
 
     f onComplete {
@@ -28,16 +30,16 @@ object Client extends App {
         val msg = Info("Hello from client")
         a ! msg
         println(s"--- C2 sent message to server: '${msg}'")
+        system.shutdown
+        println("--- Ds shutdown")
       }
-      case Failure(x) => println(s"--- C3 Completed with failure ${x}")
-    }
-    f onFailure {
-      case at: AskTimeoutException => println(s"--- C4a Received AkkaTimeoutException '${at.getMessage()}' onFailure")
-      case any => println(s"--- C4b Received Any '$any' onFailure")
+      case Failure(x) => {
+        println(s"--- C3 Completed with failure ${x}")
+        system.shutdown
+        println("--- Df shutdown")
+      }
     }
 
-    system.shutdown
-    println("--- D shutdown")
   } catch {
     case e: Exception => {
       println(s"--- E Failure in initializing the client $e")
