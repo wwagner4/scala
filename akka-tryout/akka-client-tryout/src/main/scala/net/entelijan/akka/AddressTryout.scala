2b4e6b024cfe6e880b5339d41a588bf59fba7a5f
wwagner4 <wwagner4@gmail.com>
2013-06-15 19:46:59 +0200

    access for linux

--- akka-tryout/akka-client-tryout/src/main/scala/net/entelijan/akka/AddressTryout.scala
+++ akka-tryout/akka-client-tryout/src/main/scala/net/entelijan/akka/AddressTryout.scala
@@ -3,7 +3,7 @@ package net.entelijan.akka
 import java.net._
 
 object AddressTryout extends App {
-    val name = "randy"
+    val name = "randy.lan"
 
     {
         val addrs = InetAddress.getAllByName(name);
@@ -25,10 +25,29 @@ object AddressTryout extends App {
 
     {
         val hn = java.net.InetAddress.getLocalHost().getHostName()
-        println(s"hostname='$hn'")
-        val addrs = InetAddress.getAllByName(hn);
+        val addrs = InetAddress.getAllByName(s"$hn.lan");
         println(addrs.toList.map(a => s"${a.getClass.getName} ${a.getHostName()} ${a.getHostAddress()} ${a.getCanonicalHostName()}").mkString("\n"))
         println
+    }
+    {
+        import scala.collection.JavaConversions._
+        val nifs = java.net.NetworkInterface.getNetworkInterfaces()
+        for (nif <- nifs) {
+            println(s"displayName=${nif.getDisplayName}")
+            val adrs = nif.getInetAddresses()
+            for (adr <- adrs) {
+            	println(s"adr canonical host name=${adr.getCanonicalHostName}")
+            	println(s"adr host address=${adr.getHostAddress}")
+            	println(s"adr is local=${adr.isAnyLocalAddress()}")
+            	println(s"adr is linked local=${adr.isLinkLocalAddress()}")
+            	println(s"adr is site local=${adr.isSiteLocalAddress()}")
+            	println(s"adr is loopback=${adr.isLoopbackAddress()}")
+            	println(s"adr is address=${adr.getAddress().toList}")
+            	println(s"adr is address.size=${adr.getAddress().size}")
+            	println
+            }
+           	println("----------")
+        }
     }
 
 }
